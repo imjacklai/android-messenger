c5f867e7cc37f391f429e6a4136377a6f73b964e
Jack <jacklai5505@gmail.com>
2017-05-23 11:20:35 +0800

    Setup input text field and send button.

--- app/src/main/java/tw/ctl/messenger/ChatActivity.kt
+++ app/src/main/java/tw/ctl/messenger/ChatActivity.kt
@@ -2,16 +2,34 @@ package tw.ctl.messenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
+
+    private var toUser: User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        val bundle = intent.extras
-        toolbar.title = bundle.getString("name")
+        val bundle = intent.extras ?: return
+
+        toUser = User(
+                bundle.getString("id"),
+                bundle.getString("name"),
+                bundle.getString("email"),
+                bundle.getString("profileImageUrl")
+        )
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        toolbar.title = toUser!!.name
+
+        sendButton.setOnClickListener {
+            
+        }
     }
 
 }
